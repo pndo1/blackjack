1193b4bc2a6235ff89497c8f7302341dd4c5e2f4
Ryan Mitchell <rmitchell756@gmail.com>
2018-04-02 11:01:30 -0500

    Make changes to game

--- Game.java
+++ Game.java
@@ -26,6 +26,7 @@ public class Game
     }
 
     public static void main(String [] args){
+        Scanner scan=new Scanner(System.in);
         numStartChips=10;
         player=new Player(numStartChips);
         deal=new Dealer(numStartChips);
@@ -40,10 +41,28 @@ public class Game
             System.out.println(c);
         }
         System.out.println("The value is: "+playerValue);
-        System.out.println("The dealer's second card is: ");
-        
-        
+        if(playerValue==21){
+            deal.endRound("p","blackjack");   
+        }
+        System.out.println("The dealer's second card is: "+deal.revealOpposite().get(0)+" with a value of: "+deal.revealOpposite().get(0).getCardValue());
+        if(deal.revealOpposite().get(0).getCardValue()==10){
+            if(deal.revealOpposite().get(0).getCardValue()+deal.revealHole().getCardValue()==21){
+                deal.endRound("d","blackjack");   
+            }
+        }
         System.out.println("Would you like to hit or stand?");
+        String t=scan.nextLine();
+        if(t.equals("hit"))
+            player.hit();
+        System.out.println("You have: ");
+        System.out.println("The dealer is taking his turn");
+        deal.takeTurn();
+        playerValue=0;
+        for(Card c:player.revealCards()){
+            playerValue+=c.getCardValue();
+            System.out.println(c);
+        }
+        System.out.println("The value is: "+playerValue);
         //Player takes turn
     }
 }
