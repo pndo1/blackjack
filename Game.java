6fc535bf8709806f4048989f2263daa761a970a9
Ryan Mitchell <rmitchell756@gmail.com>
2018-04-02 11:04:53 -0500

    Merged games

--- Game.java
+++ Game.java
@@ -11,57 +11,68 @@ import java.util.Scanner;
  */
 public class Game
 {
+    /*public static void play(){
+    while(numStartChips>0){
+    deal.dealCard();
+    }   
+    }*/
+
+    /*
     public static void main(String[]args){
-        Player play;
-        Dealer deal;
-        Hand hand= new Hand();
-        Scanner scan= new Scanner(System.in);
+    Player play;
+    Dealer deal;
+    Hand hand= new Hand();
+    Scanner scan= new Scanner(System.in);
+    System.out.print("Please enter a player name: ");
+    String name=scan.next();
+
+    System.out.print("Please enter the number of chips you own: ");
+    int numChips = scan.nextInt();
+
+    System.out.println("Please enter the amount of chips you would like to bet: ");
+    int betChips = scan.nextInt();
+    if(betChips%2==1){
+    System.out.println("Please enter the amount of chips you would like to bet: ");
+    betChips = scan.nextInt();
+    }
+    play= new Player(numChips);
+    deal= new Dealer(numChips);
+
+    deal.dealCard();
+
+    System.out.println("Would you like to hit or stand: ");
+    String playInput= scan.next();
+
+    if(playInput.equals("hit")){
+    play.hit();
+    }
+
+    deal.takeTurn();
+
+    if(hand.getTotalValue()>=17){
+    String dealInput="stand";
+    }
+
+    //if(playInput.
+    }*/
+
+    public static void main(String [] args){
+        Scanner scan=new Scanner(System.in);
+
         System.out.print("Please enter a player name: ");
         String name=scan.next();
 
-<<<<<<< HEAD
         System.out.print("Please enter the number of chips you own: ");
-        int numChips = scan.nextInt();
-=======
-    public static void play(){
-        while(numStartChips>0){
-            deal.dealCard();
-        }   
-    }
->>>>>>> 1193b4bc2a6235ff89497c8f7302341dd4c5e2f4
+        int numStartChips = scan.nextInt();
 
-<<<<<<< HEAD
         System.out.println("Please enter the amount of chips you would like to bet: ");
         int betChips = scan.nextInt();
         if(betChips%2==1){
             System.out.println("Please enter the amount of chips you would like to bet: ");
             betChips = scan.nextInt();
         }
-        play= new Player(numChips);
-        deal= new Dealer(numChips);
-
-        deal.dealCard();
-
-        System.out.println("Would you like to hit or stand: ");
-        String playInput= scan.next();
-
-        if(playInput.equals("hit")){
-            play.hit();
-        }
-        
-        deal.takeTurn();
-        
-        if(hand.getTotalValue()>=17){
-           String dealInput="stand";
-        }
-        
-        //if(playInput.
-=======
-    public static void main(String [] args){
-        Scanner scan=new Scanner(System.in);
-        numStartChips=10;
-        player=new Player(numStartChips);
-        deal=new Dealer(numStartChips);
+        Player player=new Player(numStartChips);
+        Dealer deal=new Dealer(numStartChips);
         player.hit();
         deal.takeTurn();
         player.hit();
@@ -96,6 +107,6 @@ public class Game
         }
         System.out.println("The value is: "+playerValue);
         //Player takes turn
->>>>>>> 1193b4bc2a6235ff89497c8f7302341dd4c5e2f4
+
     }
 }
