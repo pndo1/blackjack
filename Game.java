b05aa42367792854e88ced29ff8c74823a99a2b3
Ryan Mitchell <rmitchell756@gmail.com>
2018-04-03 11:00:01 -0500

    Added more dealer handling

--- Game.java
+++ Game.java
@@ -11,54 +11,10 @@ import java.util.Scanner;
  */
 public class Game
 {
-    /*public static void play(){
-    while(numStartChips>0){
-    deal.dealCard();
-    }   
-    }*/
-
-    /*
-    public static void main(String[]args){
-    Player play;
-    Dealer deal;
-    Hand hand= new Hand();
-    Scanner scan= new Scanner(System.in);
-    System.out.print("Please enter a player name: ");
-    String name=scan.next();
-
-    System.out.print("Please enter the number of chips you own: ");
-    int numChips = scan.nextInt();
-
-    System.out.println("Please enter the amount of chips you would like to bet: ");
-    int betChips = scan.nextInt();
-    if(betChips%2==1){
-    System.out.println("Please enter the amount of chips you would like to bet: ");
-    betChips = scan.nextInt();
-    }
-    play= new Player(numChips);
-    deal= new Dealer(numChips);
-
-    deal.dealCard();
-
-    System.out.println("Would you like to hit or stand: ");
-    String playInput= scan.next();
-
-    if(playInput.equals("hit")){
-    play.hit();
-    }
-
-    deal.takeTurn();
-
-    if(hand.getTotalValue()>=17){
-    String dealInput="stand";
-    }
-
-    //if(playInput.
-    }*/
 
     public static void main(String [] args){
         boolean playerOn=true;
-        boolean dealerOn=false;
+        boolean dealerOn=true;
         boolean win=false;
         Scanner scan=new Scanner(System.in);
 
@@ -107,7 +63,7 @@ public class Game
                 player.buyInsurance(insureChips);
             }
         }
-        while(playerOn && !win){
+        while(playerOn){
             System.out.print("\nWould you like to hit or stand? ");
             String t=scan.next();
             if(t.equals("hit")){
@@ -137,8 +93,8 @@ public class Game
         }
         System.out.println("The dealer is taking his turn");
         System.out.println("The dealer's deck is: ");
-        System.out.println(deal.revealOpposite().get(0));
-        while(!dealerOn && !win){
+        System.out.println(deal.revealHole()+"\n"+deal.revealOpposite().get(0));
+        while(dealerOn){
             deal.takeTurn();
             if(deal.getValue()==21){
                 win=true;
